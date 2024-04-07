1c2649dd660117adc397c55b96ef82611149e586
Yuriy Yuriy <you.thunderstruck@gmail.com>
2024-04-07 13:55:42 +0300

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,12 @@
 function checkForSpam(message) {
-  let spamStatus = false;
-  if (
-    message.toLowerCase().indexOf("spam") != -1 ||
-    message.toLowerCase().indexOf("sale") != -1
-  ) {
-    spamStatus = true;
-  }
-  return spamStatus;
+  const normalizeMessage = message.toLowerCase();
+
+  return normalizeMessage.includes("spam") || normalizeMessage.includes("sale");
 }
-console.log(checkForSpam("Get best sale offers now!"));
-console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
--- js/task-4.js
+++ js/task-4.js
@@ -1,23 +1,24 @@
 function getShippingCost(country) {
-  let msg = "";
   switch (country) {
     case "China":
-      msg = `Shipping to ${country} will cost 100 credits`;
-      break;
-    case "Chile":
-      msg = `Shipping to ${country} will cost 250 credits`;
-      break;
-    case "Australia":
-      msg = `Shipping to ${country} will cost 170 credits`;
-      break;
-    case "Jamaica":
-      msg = `Shipping to ${country} will cost 120credits`;
-      break;
-    default:
-      msg = `Sorry, there is no delivery to your country`;
-  }
+      return `Shipping to ${country} will cost 100 credits`;
 
-  return msg;
+    case "Chile":
+      return `Shipping to ${country} will cost 250 credits`;
+
+    case "Australia":
+      return `Shipping to ${country} will cost 170 credits`;
+
+    case "Jamaica":
+      return `Shipping to ${country} will cost 120credits`;
+
+    default:
+      return `Sorry, there is no delivery to your country`;
+  }
 }
-console.log(getShippingCost("Australia"));
-console.log(getShippingCost("Sweden"));
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
