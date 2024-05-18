fc0e969cd18522b0a70e08cdd63c358462e20e4f
Yuriy Yuriy <you.thunderstruck@gmail.com>
2024-05-18 22:29:41 +0300

    add gallery

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const categories = document.querySelectorAll('.item')
-console.log(`Number of categories: ${categories.length}`);
-categories.forEach(category => {
-   const title =  category.querySelector('h2');
-   const elements = category.querySelector('ul').querySelectorAll('li')
-   console.log(`Category: ${title.textContent}`);
-   console.log(`Elements: ${elements.length}`);
-})
--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputLink = document.querySelector("#name-input");
-let outputLink = document.querySelector("#name-output");
-
-inputLink.addEventListener("input", event => {
-   let text = event.target.value.trim();
-   outputLink.textContent = text || 'Anonymous'
- });
